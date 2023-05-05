53ef1aebf284e8191c47bec7e27c08a0359b16a0
seerat <seerat12mr@gmail.com>
2023-05-05 20:33:34 +0530

    updated

--- index.js
+++ index.js
@@ -1,18 +1,96 @@
-let listItem=document.getElementsByClassName('listitem')[0];
-let toDoList=[];
-function addToDo(){
-    let item=document.getElementById('item').value;
-    toDoList.push(item);
-    renderToDo();
-    }
-    function removeTodo(){
-        toDoList.pop(item);
-        renderToDo();
-    }
-    function renderToDo(){
-        let finalResult="";
-       toDoList.forEach(item => {
-        finalResult+=`<li>${item}</li>`;
-        listItem.innerHTML=finalResult;
-       });}
-    
+let listItem = document.getElementsByClassName("listitem")[0];
+let toDoList = [];
+/**
+ * @description this function will add todo's
+ */
+function addToDo() {
+  let toDo = document.getElementById("item").value;
+  toDoList.push(toDo);
+  renderToDo();
+}
+
+/**
+ *
+ * @param {string} discardValue
+ * @description this function will remove todo's
+ */
+function removeTodo(discardValue) {
+  toDoList = toDoList.filter((item) => item !== discardValue);
+  renderToDo();
+}
+function createToDoNode() {
+  let finalResult = "";
+  toDoList.forEach((item) => {
+    finalResult += `<li class="toDo"><span class="itemName">${item}</span><button class="removeButton" onclick="removeTodo('${item}')">
+    <span>&#9587</span></button></li>`;
+  });
+  listItem.innerHTML = finalResult;
+}
+function setPreceeder() {
+  let arr = toDoList.length;
+  let preceeder = "tasks";
+  if (arr <= 1) {
+    preceeder = "task";
+  }
+
+  document.getElementsByClassName("tasks")[0].innerHTML = `${arr} ${preceeder}`;
+}
+function renderToDo() {
+  createToDoNode();
+  setPreceeder();
+}
+let now = new Date();
+console.log(now);
+let day = now.getDay();
+console.log(day);
+function getWeekName(day) {
+  switch (day) {
+    case 0:
+      return "sunday,";
+
+    case 1:
+      return "monday,";
+
+    case 2:
+      return "tuesday,";
+
+    case 3:
+      return "wednesday,";
+
+    case 4:
+      return "thursday,";
+
+    case 5:
+      return "friday,";
+
+    case 6:
+      return "saturday,";
+      break;
+
+    default:
+      return "invalid";
+      break;
+  }
+}
+getWeekName(day);
+document.getElementsByClassName("day")[0].innerHTML = getWeekName(day);
+let date = now.getDate();
+console.log(date);
+document.getElementsByClassName("date")[0].innerHTML = date;
+let month = now.getMonth();
+console.log(month);
+let monthNames = [
+  "january",
+  "february",
+  "march",
+  "april",
+  "may",
+  "june",
+  "july",
+  "august",
+  "september",
+  "october",
+  "november",
+  "december",
+];
+document.getElementsByClassName("month")[0].innerHTML = monthNames[month];
